428fe22838cc65305fb03ca296ef19224279ee15
jakub-dydynski_controls <jakub.dydynski+controls@diehl.com>
2025-03-12 08:55:35 +0100

    Added builds generated with nrf connect extension
    Succesfully managed to flash example program to two different vendors eval boards

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Playing with Zephyr ecosystem
 
 # Lessons learnt
 - run zephyr-env.cmd to enable west workspace related commands.
+- renesas flashing went out of the box with help of flashing with button from nrf connect
+- stm flashing needed slight modification instead of "west flash -d c:\ncs\wkspc\example\blinky\build --domain blinky -i 602005882", correct command was "west flash -d c:\ncs\wkspc\example\blinky\build --domain blinky"
---
+++ renesas_eval/blinky/zephyr/zephyr.dts.d
@@ -0,0 +1,14 @@
+empty_file.o: C:/ncs/v2.9.0/zephyr/misc/empty_file.c \
+ C:/ncs/v2.9.0/zephyr/boards/renesas/ek_ra6m5/ek_ra6m5.dts \
+ C:/ncs/v2.9.0/zephyr/dts/arm/renesas/ra/ra6/r7fa6m5bh3cfc.dtsi \
+ C:/ncs/v2.9.0/zephyr/dts/arm/renesas/ra/ra6/r7fa6m5xh.dtsi \
+ C:/ncs/v2.9.0/zephyr/dts/arm/renesas/ra/ra6/ra6-cm33-common.dtsi \
+ C:/ncs/v2.9.0/zephyr/dts/common/mem.h \
+ C:/ncs/v2.9.0/zephyr/dts/arm/armv8-m.dtsi \
+ C:/ncs/v2.9.0/zephyr/dts/common/skeleton.dtsi \
+ C:/ncs/v2.9.0/zephyr/include/zephyr/dt-bindings/pinctrl/renesas/pinctrl-ra.h \
+ C:/ncs/v2.9.0/zephyr/include/zephyr/dt-bindings/clock/ra_clock.h \
+ C:/ncs/v2.9.0/zephyr/dts/common/freq.h \
+ C:/ncs/v2.9.0/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ C:/ncs/v2.9.0/zephyr/boards/renesas/ek_ra6m5/ek_ra6m5-pinctrl.dtsi \
+ C:/ncs/wkspc/example/blinky/boards/ek_ra6m5.overlay
